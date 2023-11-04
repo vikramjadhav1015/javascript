d45bcd951e42c002d16f33b671d23e6addcd39ec
vikramjadhav1015 <49741969+vikramjadhav1015@users.noreply.github.com>
2023-11-04 16:00:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all code as newer version of javascript
+
+// alert("hello"); //it will not work - we are using node.js not brower 
+
+// code readability should be high
+
+// https://tc39.es/ecma262/ for ECMA standard documentation
+
+// mdn https://developer.mozilla.org/en-US/docs/Web/API/Window/alert for documentation - not official
+
+let name = "vikram" 
+let age = 20
+let isLoggedIn =  true
+
+// primitive datatypes 
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value
+// symobol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
