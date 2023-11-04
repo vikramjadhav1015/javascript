2586cf8cf92304b3b303fd3e9ddb16f56bf50ebe
vikramjadhav1015 <49741969+vikramjadhav1015@users.noreply.github.com>
2023-11-04 17:53:54 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,8 +35,64 @@ let someNumber = 33
 
 let stringSomeNumber =  String(someNumber)
 
-console.log(typeof stringSomeNumber);
-console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+
+
+// ********** Operations **********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%3);
+// console.log(2*3);
+// console.log(2**3);  // ** for power 2^3
+
+let str1 = "hello"
+let str2 = " vikram"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32 ***
+
+// console.log(true); //true
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+console.log(gameCounter);
+
+gameCounter++ // x++, the increment operator increments and returns the value before incrementing.
+console.log(gameCounter);
+
+++gameCounter // ++x, the increment operator increments and returns the value after incrementing.
+console.log(gameCounter);
+
+/*
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+*/
 
 
 
