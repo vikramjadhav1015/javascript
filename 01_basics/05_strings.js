edeb3af770c203b903d98b38d2b71132fc423994
Vikram Jadhav <vikramjadhav1015@gmail.com>
2023-11-05 21:02:20 +0530

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name = "vikram"
+const count = 40
+
+// console.log(name + count + " value");  // do not use this
+
+// console.log(`Hello, my name is ${name} and my repo count is ${count}`);  //**IMP - use this */
+
+// const myName = new String("vikram")
+// console.log(myName);
+// console.log(myName[0]);
+// console.log(name[0]);
+// console.log(myName.__proto__);
+// console.log(myName.length);
+// console.log(myName.toUpperCase()); // it will create new copy in stack and not change existing value
+// console.log(myName.charAt(2));
+// console.log(myName.indexOf('m'));
+
+
+// const gameName = "vikram-jadhav"
+// let newString =  gameName.substring(0,2)  //last character not included
+// console.log(newString);
+
+// let anotherString  = gameName.slice(0,2)//last character not included
+// let anotherString2  = gameName.slice(-8,4)
+
+// console.log(anotherString2);
+
+
+let newString1 = " vikram    jadhav  \n "
+// console.log(newString1);
+// console.log(newString1.trim());  // also remove last \n - new line
+
+let url = "abc@gmail.com"
+
+console.log(url.replace("abc", "xyz"));
+console.log(url.includes("abc"));
+
+console.log(url.split('@'));
+
+// split javascript mdn
+
+
